35271cda6c58c54474aa2d91d5912cb6d4fb28e7
Alexey Kott <alexey.kott@gmail.com>
2018-05-09 16:14:28 +0300

    Output in draft added

--- extract.py
+++ extract.py
@@ -1,7 +1,16 @@
 import pickle
 import sys
+import os
 
 extract_file = sys.argv[1]
+os.remove('draft.txt')
+
 with open(extract_file, 'rb') as file:
 	dataset = pickle.load(file)
-	print(dataset)
+	for item in dataset:
+		print(item['date'])
+		print(item['url'])
+		print(item['text'], end="\n\n")
+
+		with open('draft.txt', 'a') as f:
+			f.write(item['text'])
