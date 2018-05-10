6074dde78630198601a49b6b6f74725b4e59f318
Alexey Kott <alexey.kott@gmail.com>
2018-05-11 01:27:12 +0300

    Fix encoding in extract

--- extract.py
+++ extract.py
@@ -3,14 +3,13 @@ import sys
 import os
 
 extract_file = sys.argv[1]
-os.remove('draft.txt')
 
-with open(extract_file, 'rb') as file:
-	dataset = pickle.load(file)
-	for item in dataset:
-		print(item['date'])
-		print(item['url'])
-		print(item['text'], end="\n\n")
+with open('draft.txt', 'w', encoding='utf-8') as fwrite:
+	with open(extract_file, 'rb') as fread:
+		dataset = pickle.load(fread)
+		for item in dataset:
+			print(item['date'])
+			print(item['url'])
+			print(item['text'], end="\n\n")
 
-		with open('draft.txt', 'a') as f:
-			f.write(item['text'])
+			fwrite.write("{}\n{}\n{}\n\n".format(item['date'], item['url'], item['text']))
