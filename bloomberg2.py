b1a8dd24e60e0051fad66665ce428a2c14169a0f
Alexey Kott <alexey.kott@gmail.com>
2018-05-23 08:17:07 +0300

    395 bloomberg news

--- bloomberg2.py
+++ bloomberg2.py
@@ -1,9 +1,22 @@
 import requests as req
 from bs4 import BeautifulSoup
 import pickle
+from time import sleep
+
+pkl_file = 'bloomberg.pkl'
+
+def save(data):
+	with open(pkl_file, 'rb') as file:
+		dataset = pickle.load(file)
+		dataset.append(data)
+	with open(pkl_file, 'wb') as file:
+		pickle.dump(dataset, file)
 
 
 if __name__ == "__main__":
+	with open(pkl_file, 'wb') as file:
+		dataset = []
+		pickle.dump(dataset, file)
 	url = "https://www.bloomberg.com/search"
 	n = 430
 	dataset = []
@@ -13,15 +26,18 @@ if __name__ == "__main__":
 			'page': i
 		}
 		page = req.get(url, params=params)
-		print(page.url)
+		print("URL: {}".format(page.url))
 		soup = BeautifulSoup(page.text, "lxml")
 
 		for item in soup.find_all(class_="search-result"):
 			try:
 				h1 = item.find("h1")
 				news_url = h1.a['href']
+				print(news_url)
 
 				if news_url.find('/audio/') != -1:
+					continue
+				if news_url.find('/videos/') != -1:
 					continue
 
 				news_page = req.get(news_url)
@@ -34,15 +50,15 @@ if __name__ == "__main__":
 				for p in fence_body.find_all("p"):
 					text += p.text
 
-				dataset.append({
+				data = {
 					'url': news_url,
 					'date': datetime,
 					'text': text
-					})
-			except:
-				continue
+					}
+				save(data)
+			except Exception as e:
+				# raise e
+				print("{}\n{}".format(e, news_url), end="\n\n")
+			sleep(1)
 
 		
-
-	with open('bloomberg.pkl', 'wb') as f:
-		pickle.dump(dataset, f)
--- extract.py
+++ extract.py
@@ -7,9 +7,10 @@ extract_file = sys.argv[1]
 with open('draft.txt', 'w', encoding='utf-8') as fwrite:
 	with open(extract_file, 'rb') as fread:
 		dataset = pickle.load(fread)
+		print(len(dataset))
 		for item in dataset:
-			print(item['date'])
-			print(item['url'])
-			print(item['text'], end="\n\n")
+			# print(item['date'])
+			# print(item['url'])
+			# print(item['text'], end="\n\n")
 
 			fwrite.write("{}\n{}\n{}\n\n".format(item['date'], item['url'], item['text']))
